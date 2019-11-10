4e8fea217a045f2870c147a357b67cae793a84ac
Miguel Wolmers <rakasmurfs@gmail.com>
2019-11-10 15:31:34 -0500

    Updates

---
+++ precache-manifest.206049dfbcadeceb276ef0e49484ce19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d3142b9ae326fba19f51df63906c35",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "2fc07c76886cb2de6e44",
+    "url": "/clickyGame/static/css/main.ec1dbfd5.chunk.css"
+  },
+  {
+    "revision": "f7f80829525ccccf4fdd",
+    "url": "/clickyGame/static/js/2.201a6521.chunk.js"
+  },
+  {
+    "revision": "2fc07c76886cb2de6e44",
+    "url": "/clickyGame/static/js/main.2fecdb6f.chunk.js"
+  },
+  {
+    "revision": "b86591952698d412d003",
+    "url": "/clickyGame/static/js/runtime-main.db88dee0.js"
+  }
+]);
